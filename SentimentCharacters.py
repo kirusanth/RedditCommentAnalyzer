b9d365733ca799f34c84709b30e032f7a502b902
kirusanth <tkirusanth@gmail.com>
2018-11-28 03:06:06 -0500

    Add files via upload

---
+++ SentimentCharacters.py
@@ -0,0 +1,21 @@
+
+from pyspark import SparkConf,SparkContext
+from pyspark.streaming import StreamingContext
+from pyspark.sql import Row,SQLContext, SparkSession
+import sys
+import requests
+import re
+
+# create spark configuration
+conf = SparkConf()
+conf.setAppName("TwitterStreamApp")
+# create spark context with the above configuration
+sc = SparkContext(conf=conf)
+
+spark =  SparkSession.builder.config(conf=sc.getConf()).getOrCreate()
+
+episode_csv= "episode1.csv"
+path= "../GameofThrones/"+ episode_csv
+
+
+df = spark.read.format("csv").option("header", "true").load(path).show(5)
